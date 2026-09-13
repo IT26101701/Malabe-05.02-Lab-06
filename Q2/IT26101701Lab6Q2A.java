d53f25c2b36c63df8aa2d10fb9c74db2b7c874fd
IT26101701 <it26101701@my.sliit.lk>
2026-09-13 18:48:07 +0530

    Add Lab6Q2A/ Lab6Q2B/ Lab6Q2C to Q2

---
+++ Q2/IT26101701Lab6Q2A.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class IT26101701Lab6Q2A {
+	
+    public static void main(String[] args) {
+		
+		//Declaring and Initializing the counter variable
+		int counter = 1;
+		
+		//Using a while loop to print number from 1 to 10
+		while (counter <= 10) {
+			
+			//Printing the current number followed by a space
+			System.out.print(counter + " ");
+			counter++;		//Incrementing the counter
+			
+		}
+		
+		// Printing a newline character to move to the next line after printing all numbers
+		System.out.println();
+        
+    }
+}
